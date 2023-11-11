6d4d9acdabe5b8f21818be6463ec7cecf877cb80
Heehyun-Ryu <rhh221@naver.com>
2023-11-11 19:49:41 +0900

    "update classification folder and update JSON file"

--- indxe.js
+++ indxe.js
@@ -4,7 +4,8 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const data = JSON.parse(fs.readFileSync("./photo.json", "utf-8"));
+const upload_json = JSON.parse(fs.readFileSync("./photo.json", "utf-8"));
+const classificaion_json = JSON.parse(fs.readFileSync("./classification.json", "utf-8"));
 
 // const upload = multer({
 //     storage: multer.diskStorage({
@@ -65,8 +66,13 @@ app.post('/upload', upload.array("image", 10), (req, res) => {
     res.send('Files uploaded!');
 });
 
+//accese the photo through '/example/cat.jpg'
 app.use('/example', express.static('upload'));
 
+//accese the photo through '/exam/classification/cluster1/cat.jpg
+app.use('/exam', express.static('classification'));
+
+//update upload folder and new release photo.json
 function exportImagesToJson(){
     const uploadPath = path.join(__dirname, 'upload');
     const pictures= []
@@ -85,6 +91,41 @@ function exportImagesToJson(){
     console.log('images export to json');
 }
 
+//update classification folder and new release classification.json
+function exportImagesToJson_classification(){
+    const classification_path = path.join(__dirname, 'classification');
+    const clusters = [];
+
+    fs.readdirSync(classification_path).forEach((folder) => {
+        console.log(folder);
+        imgList = [];
+
+        fs.readdirSync(path.join(__dirname, 'classification', folder)).forEach((img) => {
+            // img = img.split(' ').join('');
+            console.log(img);
+            if(img.endsWith('.jpg') || img.endsWith('.png')){
+                imgList.push({name: img, path: `/classificaion/${folder}/${img}`});
+            }
+            
+        })
+        clusters.push({[folder]: imgList});
+    });
+
+    console.log(clusters);
+    
+    const jsonFile = JSON.stringify(clusters, null, 2);
+    console.log(path.join(__dirname, 'classification'));
+    console.log(jsonFile);
+    fs.writeFileSync(path.join(__dirname, `classification.json`), jsonFile);
+}
+// exportImagesToJson_classification();
+
+// detect folder update and notify
+fs.watch(`./classification`, {recursive: true}, (eventType, filename) => {
+    console.log(`File ${filename} has been ${eventType}`);
+    exportImagesToJson_classification();
+});
+
 //detect folder update and notify
 fs.watch('./upload', {recursive: false}, (eventType, filename) => {
     console.log(`File ${filename} has been ${eventType}`);
@@ -95,8 +136,12 @@ app.use(express.json());
 
 app.get("/point", (req, res) => {
     // res.send('fuck you');
-    res.json(data);
+    res.json(upload_json);
 });
+
+app.get("/classify", (req, res) => {
+    res.json(classificaion_json);
+})
 
 app.listen(8080, () => {
     console.log('listen on 8080');
